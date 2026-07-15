26083e4983c6b76c877e6aeb70a5f0404717857c
Andrei Dubov <av.dubov@yandex.ru>
2026-07-15 16:30:07 +0400

    chore(interfaces): add router

---
+++ app/interface/router.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+
+class RouterInterface(ABC):
+    """Интерфейс роутера для навигации"""
+
+    @abstractmethod
+    def start(self) -> None:
+        """Запускает приложение (показывает главное окно)"""
+        ...
+
+    @abstractmethod
+    def show_log_window(self) -> None:
+        """Открывает или активирует окно логов"""
+        ...
+
+    @abstractmethod
+    def close_all(self) -> None:
+        """Закрывает все окна и завершает приложение"""
+        ...
+
+    @abstractmethod
+    def hide_splash_screen(self) -> None:
+        """Скрывает сплэш-скрин (если есть)"""
+        ...
