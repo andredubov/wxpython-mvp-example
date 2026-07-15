c909a7fda6822896e61a74ed99eeb17da73f3135
Andrei Dubov <av.dubov@yandex.ru>
2026-07-15 16:29:02 +0400

    chore(interfaces): add repositories

---
+++ app/interface/repository.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class RepositoryProtocol(ABC):
+    """Интерфейс репозитория для работы с данными"""
+
+    @abstractmethod
+    def load_value(self) -> int:
+        """Загружает сохраненное значение счетчика"""
+        ...
+
+    @abstractmethod
+    def save_value(self, value: int) -> None:
+        """Сохраняет значение счетчика"""
+        ...
